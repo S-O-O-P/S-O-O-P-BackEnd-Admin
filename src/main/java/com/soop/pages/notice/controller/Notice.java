a48b6421efc64d7746073f01650848b95e6431a3
rlawlsdyd5932 <jyjy0509@naver.com>
2024-07-05 11:50:44 +0900

    commit

--- src/main/java/com/soop/pages/notice/controller/Notice.java
+++ src/main/java/com/soop/pages/notice/controller/Notice.java
@@ -1,20 +1,41 @@
 package com.soop.pages.notice.controller;
 
+import com.soop.pages.customer.model.dto.LinkBeeUserDTO;
 import com.soop.pages.notice.model.dto.NoticeFileDTO;
 import com.soop.pages.notice.model.service.NoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/notice")
 public class Notice {
 
     @Autowired
     private NoticeService noticeService;
+
+
+    @GetMapping("/")
+    public ResponseEntity<Map<String,Object>> getNoticeList() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        List<NoticeFileDTO> noticeInfo = noticeService.getNoticeList();
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("noticeInfo", noticeInfo);
+        return new ResponseEntity<>(responseMap, headers, HttpStatus.OK);
+    }
+
+
     @PostMapping("/noticeregist")
     public ResponseEntity<?> registNotice(@RequestBody NoticeFileDTO noticeFileDTO) {
 
--- src/main/java/com/soop/pages/notice/model/dao/NoticeMapper.java
+++ src/main/java/com/soop/pages/notice/model/dao/NoticeMapper.java
@@ -3,7 +3,11 @@ package com.soop.pages.notice.model.dao;
 import com.soop.pages.notice.model.dto.NoticeFileDTO;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface NoticeMapper {
     void registNotice(NoticeFileDTO noticeFileDTO);
+
+    List<NoticeFileDTO> getNoticeList();
 }
